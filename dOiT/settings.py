b548fda43f409a497389a4fff054a9629992aa11
Walid Zakaria <walidpiano@yahoo.com>
2019-09-25 02:21:47 +0200

    Fix: change to postgresql

--- dOiT/settings.py
+++ dOiT/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'dOiT.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "d20kmhhatbl1j",
+        "USER": "gxbyyeekrxmwvr",
+        "PASSWORD": "05cc69f80577a6159d74b5094f7b6646eb65615e029e20c006d12d253f82bf96",
+        "HOST": "ec2-107-20-168-237.compute-1.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
